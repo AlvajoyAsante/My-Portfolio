c52d95d2c7e30bd5944b8987c6d45d5bb9f7845c
Alvajoy Asante <alvajoy123@gmail.com>
2025-05-07 23:59:55 -0400

    initial commit

---
+++ personal_portfolio/pages/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    context = {
+        'Firstname': 'Alvajoy',
+        'Lastname': 'Asante',
+        'Fullname': 'Alvajoy Asante',
+        'Phonenumber': '(980) 288-4855',
+        'Email': 'aasante@charlotte.edu',
+        'Location': 'University of Charlotte, Charlotte, NC',
+        'Sitename': 'mywebsite.com',
+        'Occupation': 'Aspiring Software Engineer',
+        'LinkedIn': 'https://www.linkedin.com/in/alvajoy-asante/',
+        'Github': 'https://github.com/Overload02',
+        'Sitename': 'Alvajoy Asante',
+    }
+
+    return HttpResponse(render(request, "pages/home.html", context))
