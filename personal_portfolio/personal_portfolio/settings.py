a9a9a8bb2680dbd0a90f54a2e37f9a3d0a36ec34
AlvajoyAsante <alvajoy123@gmail.com>
2025-12-17 05:43:09 -0500

    Add sitemap functionality, improve email handling, and enhance SEO metadata

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
@@ -148,4 +149,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+if EMAIL_HOST_USER:
+    EMAIL_HOST_USER = EMAIL_HOST_USER.strip()
+    
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+if EMAIL_HOST_PASSWORD:
+    EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD.strip()
