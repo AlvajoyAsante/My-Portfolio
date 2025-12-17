5040012a43e996b336b51f07d249a3842c0c7b9b
AlvajoyAsante <alvajoy123@gmail.com>
2025-12-17 05:52:08 -0500

    Add security settings for SSL and secure cookies in production

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -34,6 +34,11 @@ CSRF_TRUSTED_ORIGINS = [
     "https://alvajoyasante.com",
 ]
 
+# Trust the X-Forwarded-Proto header for SSL (required for Cloud Run)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
